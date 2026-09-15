494a5096e86d45d2645361b25174030663542616
jhefersonescateu <jhefersonescateu@gmail.com>
2026-09-15 15:11:18 -0500

    fix: avoid forcing Google consent

--- api/index.py
+++ api/index.py
@@ -133,7 +133,6 @@ def google_login():
     auth_url, _ = flow.authorization_url(
         access_type="offline",
         include_granted_scopes="true",
-        prompt="consent",
     )
     return jsonify({"url": auth_url})
 
